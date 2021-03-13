b33fdb20b497d94c14ba0dede75e296eded287bb
Maximiliano Vicente <vicente.maximilianoh@gmail.com>
2021-03-13 12:53:31 -0300

    Fixed E2E tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('todo-ng app is running!');
+    expect(await page.getTodosList()).toBeTruthy();
   });
 
   afterEach(async () => {
